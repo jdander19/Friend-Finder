34b205a00635863c4d30a4d59753ef6867674e97
Jesse Anderson <lockbox20@gmail.com>
2020-03-07 14:45:47 -0600

    heroku ready

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
 var app = express(); 
-var port = process.env.PORT || 3000; 
+var PORT = process.env.PORT || 3000; 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(express.static('app/public'));
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
 
-app.listen(PORT, ()=> {
+app.listen(PORT, function() {
     console.log(`Connected on port: ${PORT}`);
 });
